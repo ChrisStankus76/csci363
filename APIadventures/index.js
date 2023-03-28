32ba9d0c1afe53af05f09d840cc82ee402205fe8
Chris Stankus <cps017@bucknell.edu>
2023-03-28 10:39:40 -0400

    APIadventures assignment

---
+++ APIadventures/index.js
@@ -0,0 +1,19 @@
+function getNewDog() {
+	//var url = "https://dog.ceo/api/breeds/image/random"
+    var url = "https://dog.ceo/api/breed/hound/images/random"
+
+	console.log("making fetch to", url)
+	
+	fetch(url)
+		.then(resp=>{return resp.json()})
+		.then(json=>{
+			console.log(json)
+
+			document.getElementById("dogImage").src = json.message
+		})
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+});
